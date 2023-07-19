4725c11b0c25479775edeb0c3812afd92cb2d166
Polyana Oliveira <polyanasousa127@gmail.com>
2023-07-18 23:52:55 -0300

    Adição do try catch

--- primeiro-projeto-java/src/AboutMe.java
+++ primeiro-projeto-java/src/AboutMe.java
@@ -1,24 +1,29 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        // Criando um objeto scanner
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite seu nome: ");
-        String nome = scanner.next();
-
-        System.out.print("Digite seu sobrenome: ");
-        String sobrenome = scanner.next();
-
-        System.out.print("Digite sua idade: ");
-        int idade = scanner.nextInt();
-
-        System.out.print("Digite sua altura: ");
-        double altura = scanner.nextDouble(); 
-
-        System.out.print("Olá, me chamo " + nome + " " + sobrenome + "\r\n");
-        System.out.print("Tenho " + idade + " anos" + "\r\n");
-        System.out.print("Minha altura é: " + altura);
+        try {
+            // Criando um objeto scanner
+            Scanner scanner = new Scanner(System.in);
+    
+            System.out.print("Digite seu nome: ");
+            String nome = scanner.next();
+    
+            System.out.print("Digite seu sobrenome: ");
+            String sobrenome = scanner.next();
+    
+            System.out.print("Digite sua idade: ");
+            int idade = scanner.nextInt();
+    
+            System.out.print("Digite sua altura: ");
+            double altura = scanner.nextDouble(); 
+    
+            System.out.print("Olá, me chamo " + nome + " " + sobrenome + "\r\n");
+            System.out.print("Tenho " + idade + " anos" + "\r\n");
+            System.out.print("Minha altura é: " + altura);
+        } catch(InputMismatchException e) {
+             System.out.print("O campo idade deve ser númerico.");
+        }
     }
 }
